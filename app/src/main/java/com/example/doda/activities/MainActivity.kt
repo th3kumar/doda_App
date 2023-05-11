2110913d73b073ded4beb54fdfd354cf56c833d5
Mantu Kumar Ojha <mk420464@gmail.com>
2023-05-11 16:00:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/doda/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.doda.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.doda.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
